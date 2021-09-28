df59a584651e32a322c84f2c21eafedc35821044
Roman Brunka <rbrunka@gmail.com>
2021-09-28 14:08:52 +0000

    working multicast

--- knxd/entrypoint.sh
+++ knxd/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-knxd -e 1.1.90 -E 1.1.91:8 -D -T -R -S -B log -B single -b ft12cemi:/dev/ttyAMA0
+#knxd -e 1.1.90 -E 1.1.91:8 -D -T -R -S -B log -B single -b ft12cemi:/dev/ttyAMA0
+knxd -e 1.1.90 -E 1.1.91:8 -DTRS -B log -B single -b ft12cemi:/dev/ttyAMA0
+#tail -f /dev/null
